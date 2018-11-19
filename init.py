20cf8fb1c2ceb4e03f44dc884c87111e4cbdc2a7
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-11-19 08:52:20 -0800

    testing

--- init.py
+++ init.py
@@ -1,8 +1,5 @@
-# this is called by docker run
-#
-# starts jenkins
-# installs plugins
-# adds github based projects to jenkins/jobs
+# this is called by docker run CMD
+# any software that may be installed during the docker build process should continue being installed w/ that process
 
 import http.client
 import os
@@ -11,11 +8,14 @@ import socket
 import subprocess
 import time
 
+def install_software():
+  pass
+
 def start_services():
   print("starting cron")
   subprocess.run(["service", "cron", "start"])
   time.sleep(10)
-  #subprocess.run(["mongod"])
+  subprocess.run(["mongod"])
 
 def main():
   while True:
